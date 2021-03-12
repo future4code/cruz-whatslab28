4f5667c1d4b5b09b8307d5e611072b5fd67c3906
Thiago Jatobá <tmjatob@gmail.com>
2021-03-12 15:38:07 -0300

    WhatsLab Inputs Component Created

--- whatslab/src/components/Inputs/Inputs.js
+++ whatslab/src/components/Inputs/Inputs.js
@@ -1,6 +1,42 @@
 import React from 'react'
+import Inputs from './Inputs.css'
 import styled from 'styled-components'
+import App from '../../App';
 
-class Input extends React.Component {
-    
+class Inputs extends React.Component {
+    state = {
+        mensage: [],
+        userName: "",
+        userMessage: "",
+    };
 }
+
+handleUsuario = (event) => {
+    this.setState({ userName: event.target.value });
+};
+
+handleMensagem = (event) => {
+    this.setState({ userMessage: event.target.value });
+};
+
+sendMessage = () => {
+    const newMsg = {
+        userName: this.state.userName,
+        userMesage: this.state.userMessage,
+    };
+    this.setState({ 
+        message: [...this.state.message, newMsg] 
+    });
+};
+    render() {
+    const msgList = this.state.message.map((msg, index) => {
+    return (
+        key={index}
+        userName={msg.userName}
+        userMessage={msg.userMessage}
+        );
+    });
+}
+
+export default Inputs;
+export deafult App
